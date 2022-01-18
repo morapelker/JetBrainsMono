3bf945bae13a829610656cde242d60062aae0113
morapelker <morapelker@gmail.com>
2022-01-18 21:01:53 +0200

    changes

--- sources/build.sh
+++ sources/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-fontmake JetBrainsMono-Italic.glyphs -o otf
-fontmake JetBrainsMono.glyphs -o otf
+fontmake JetBrainsMono-Italic.glyphs -o otf -a
+fontmake JetBrainsMono.glyphs -o otf -a
 cp -a master_otf/. /Users/mor/Library/Fonts/
